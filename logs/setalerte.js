a7a062522b630c137454b930fdc6e65185d06f4f
Tristan Miche <tmiche@ippon.fr>
2023-08-23 21:08:50 +0200

    fix setalert

--- logs/setalerte.js
+++ logs/setalerte.js
@@ -15,7 +15,7 @@ module.exports = {
     description: `Permet de changer le salon d'alerte.`,
     async execute(client, message, args) {
 
-        if (owner.get(`owners.${message.author.id}`) || config.app.owners.includes(message.author.id) || config.app.funny.includes(message.author.id) === true) {
+        if (await owner.get(`owners.${message.author.id}`) || config.app.owners.includes(message.author.id) || config.app.funny.includes(message.author.id) === true) {
 
             let color = await cl.get(`color_${message.guild.id}`)
             if (color == null) color = config.app.color
@@ -23,9 +23,9 @@ module.exports = {
             const newChannel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0] || message.channelId);
             if (args[0] == undefined) args[0] = `<#${message.channel.id}>`
             if (!newChannel) return message.channel.send({ content: "Aucun salon trouvé !" })
-            if (alerte.get(`${message.guild.id}.alerteperm`) === newChannel) return message.channel.send(`${emotes.owner.alert}・__Nouveau salon d'alerte :__ \`${alerte.get(`${message.guild.id}.alerteperm`)}\``)
+            if (await alerte.get(`${message.guild.id}.alerteperm`) === newChannel) return message.channel.send(`${emotes.owner.alert}・__Nouveau salon d'alerte :__ \`${alerte.get(`${message.guild.id}.alerteperm`)}\``)
             else {
-                alerte.set(`${message.guild.id}.alerteperm`, newChannel.id)
+                await alerte.set(`${message.guild.id}.alerteperm`, newChannel.id)
                 message.channel.send(`${emotes.owner.alert}・__Nouveau salon d'alerte :__ ${args[0]}`)
 
                 const embed = new Discord.MessageEmbed()
@@ -33,7 +33,7 @@ module.exports = {
                     .setDescription(`${emotes.owner.iconsettings} Ce salon est désormais utilisé pour __toutes__ **les Alertes Permissions** du serveur\n Executeur : <@${message.author.id}>`)
                     .setTimestamp()
                     .setFooter({ text: `${footer}` })
-                client.channels.cache.get(alerte.get(`${message.guild.id}.alerteperm`)).send({ embeds: [embed] }).catch(() => false)
+                client.channels.cache.get(await alerte.get(`${message.guild.id}.alerteperm`)).send({ embeds: [embed] }).catch(() => false)
             }
         }
     }
